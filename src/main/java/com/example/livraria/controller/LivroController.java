57dad7ab0ffc5285b41dc989cac85b35b8fb7e34
Marcos-Sobral <vini15oficial@gmail.com>
2025-03-31 01:25:19 -0300

    Refactor LivroController with CRUD operations

--- src/main/java/com/example/livraria/controller/LivroController.java
+++ src/main/java/com/example/livraria/controller/LivroController.java
@@ -1,5 +1,72 @@
 package com.example.livraria.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.livraria.model.Livro;
+import com.example.livraria.service.LivroService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+
+@RestController
+@RequestMapping("/livros")
 public class LivroController {
 
+    @Autowired
+    private LivroService livroService;
+
+    @GetMapping
+    public List<Livro> listarTodos(){
+        return livroService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Livro> buscarPorId(@PathVariable Long id) {
+        Optional<Livro> livro = livroService.BuscarPorId(id);
+        return livro.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+    
+    @PostMapping
+    public Livro salvar(@RequestBody Livro livro) {
+        return livroService.salvar(livro);
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Livro> atualizar(@PathVariable Long id, @RequestBody Livro livroAtualizado) {
+        Optional<Livro> livroExistente = livroService.BuscarPorId(id);  
+        
+        if (livroExistente.isPresent()) {
+            Livro livro = livroExistente.get();
+            livro.setTitulo(livroAtualizado.getTitulo());
+            livro.setAutor(livroAtualizado.getAutor());
+            livro.setPreco(livroAtualizado.getPreco());
+
+            return ResponseEntity.ok(livroService.salvar(livro));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable Long id){
+        if (livroService.BuscarPorId(id).isPresent()) {
+            livroService.excluir(id);
+            return ResponseEntity.noContent().build();
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
